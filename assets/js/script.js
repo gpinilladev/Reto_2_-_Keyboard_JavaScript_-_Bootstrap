fd92f5e2d3ee84565c3f12c0c28348b521c6034a
German Pinilla <germanpinilla@iMac-de-German.local>
2021-03-17 19:43:06 -0500

    Primer commit reto 2 Teclado con JavaScript y Bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Variables
+
+
+// Letras primera linea
+let btnEsc = document.getElementById("btnEsc");
+let btnF1 = document.getElementById("btnF1");
+let btnF2 = document.getElementById("btnF2");
+let btnF3 = document.getElementById("btnF3");
+let btnF4 = document.getElementById("btnF4");
+let btnF5 = document.getElementById("btnF5");
+let btnF6 = document.getElementById("btnF6");
+let btnF7 = document.getElementById("btnF7");
+let btnF8 = document.getElementById("btnF8");
+let btnF9 = document.getElementById("btnF9");
+let btnF10 = document.getElementById("btnF10");
+let btnF11 = document.getElementById("btnF11");
+let btnF12 = document.getElementById("btnF12");
+let btnEject = document.getElementById("btnEject");
+// Letras segunda linea
+
+
